1d8a4bd77a69447708c43129ee8a8fe5bfc54b43
CYBER CRACKER OFFICIAL <78842905+cc-cybercracker-official@users.noreply.github.com>
2021-07-01 19:28:17 +0530

    Add files via upload

---
+++ cc_sim_crack.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import time
+import random
+import urllib.request
+
+def banner():
+
+    os.system('''
+    printf "\n"
+    printf "     ██████╗ ██████╗    ███████╗██╗███╗   ███╗     ██████╗██████╗  █████╗  ██████╗██╗  ██╗  \n"
+    printf "    ██╔════╝██╔════╝    ██╔════╝██║████╗ ████║    ██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝  \n"
+    printf "    ██║     ██║         ███████╗██║██╔████╔██║    ██║     ██████╔╝███████║██║     █████╔╝   \n"
+    printf "    ██║     ██║         ╚════██║██║██║╚██╔╝██║    ██║     ██╔══██╗██╔══██║██║     ██╔═██╗   \n"
+    printf "    ╚██████╗╚██████╗    ███████║██║██║ ╚═╝ ██║    ╚██████╗██║  ██║██║  ██║╚██████╗██║  ██╗  \n"
+    printf "     ╚═════╝ ╚═════╝    ╚══════╝╚═╝╚═╝     ╚═╝     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝  \n"
+    printf "                                                                          BY CYBER CRACKER  \n\n"
+    printf "\e[1;32m                   ╔═<><><><><><><><><><><><><><><><><><><><><><><><><><><>═╗\n"
+    printf "\e[1;32m                   ║    \e[0m\e[1;95m OTP and CALL Bombing POWERFULL CRACKING SCRIPT     \e[1;32m║\n"
+    printf "\e[1;32m                              \e[0m\e[1;95m Developed by:\e[31m CYBER CRACKER OFFICIAL      \n"
+    printf "\e[1;32m                   ║\e[0m\e[1;95m  ONLY INDIAN MOBILE NUMBERS ARE CURRENTLY SUPPOTED NOW \e[1;32m║\n"
+    printf "\e[1;32m                   ╚═<><><><><><><><><><><><><><><><><><><><><><><><><><><>═╝\n"
+    printf "\n"
+    printf "                   \e[101m\e[1;77mㅤ                                                      ㅤ\e[0m\n"
+    printf "                   \e[101m\e[1;77mㅤ    DISCLAIMER:~ Developer will not be responsible    ㅤ\e[0m\n"
+    printf "                   \e[101m\e[1;77mㅤ    for any misuse or damage caused by this script    ㅤ\e[0m\n"
+    printf "                   \e[101m\e[1;77mㅤ    Please do not use this script for Revenge.        ㅤ\e[0m\n"
+    printf "                   \e[101m\e[1;77mㅤ                                                      ㅤ\e[0m\n"
+    printf "\n"
+    printf "\e[1;32m                   [*]ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴘᴛɪᴏn :\n"
+    printf "\n"
+    printf "                                       [1] sᴘᴇʀᴍ ᴀᴛᴛᴀᴄᴋ \n"
+    printf "\n"
+    printf "                                       [2] ᴀʙᴏᴜᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ \n"
+    printf "\n"
+    printf "                                       [3] ᴜᴘᴅᴀᴛᴇ \n"
+    printf "\n"
+    printf "\e[0m\e[1;95m*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\e[1;32m\n\n"
+        ''')
+banner()
+choice = input('      ᴇɴᴛᴇʀ ᴀɴ ᴏᴘᴛɪᴏɴ : ')
+
+if choice == "1":
+   os.system("python3 cc_main")
+
+elif choice == "2":
+   os.system("python3 cc_about")
+elif choice == "3":
+   os.system("termux-open-url https://instagram.com/cybercrackerofficial")
+else:
+    print ("invild option try again !")
+
+#  DONT TRY TO REEDIT MY CODE ITS MAYBE TRACABLE AND PANNISHABLE !!!
